0e0a6eef687b2aa27e9f8c5e839dfc5a5b4b4f77
ludakas <ludanev@gmail.com>
2017-04-16 00:50:45 +0100

    some code claning

--- main.py
+++ main.py
@@ -97,10 +97,10 @@ class File2DocSpace(Doc2Vec):
             self.min_alpha = self.alpha  # fix the learning rate, no decay
 
 
-with open ('word_model_20_20.pkl', 'rb') as fp:
+with open ('models/word_model_20_20.pkl', 'rb') as fp:
     trained_model_word = pkl.load(fp)
 
-def load_data(file_name="training_data.txt"):
+def load_data(file_name="data/training_data.txt"):
     """
     Places data in list of labelled sentences format which
     allows gensim to build vocab automatically
@@ -121,7 +121,7 @@ def load_data(file_name="training_data.txt"):
 
 
 
-def load_test_data(file_name="test_data.txt"):
+def load_test_data(file_name="data/test_data.txt"):
     """
     Places data in list of labelled sentences format which
     allows gensim to build vocab automatically
@@ -143,7 +143,7 @@ def load_test_data(file_name="test_data.txt"):
 data, labels = load_data()
 test_data = load_test_data()
 
-model_word = File2DocSpace("training_data.txt", epochs=0, workers=4, size=20)
+model_word = File2DocSpace("data/training_data.txt", epochs=0, workers=4, size=20)
 model_word.X = trained_model_word[0]
 model_word.y = trained_model_word[1]
 
@@ -184,17 +184,14 @@ model.compile(optimizer='adagrad',
 
 X_train, X_test, y_train, y_test = train_test_split(mat2, model_word.y, test_size=0.2, random_state=42)
 print y_test
-y_test = ~ y_test.astype(bool)
-y_test = y_test.astype(int)
 
 h = model.fit(X_train, to_categorical(y_train),
           nb_epoch=100, batch_size=500, validation_data=(X_test, to_categorical(y_test)))
 
 print model.predict_classes(test_mat)
-final_results = 1 - model.predict_classes(test_mat)
+final_results = model.predict_classes(test_mat)
 
 
 # save labels
-with open("final_results.pkl", "wb") as f:
-    pkl.dump(final_results, f)
-# {'relu 200': 0.75269913016523449, 'sigmoid 150': 0.53575066884876776, 'relu 150': 0.75188429193162554, 'sigmoid 50': 0.54114890600032728, 'sigmoid 200': 0.53625993535338456, 'relu 100': 0.73833773281356185, 'sigmoid 100': 0.53401915136824119, 'tanh 100': 0.53625993705325226, 'tanh 150': 0.53595437370217924, 'tanh 200': 0.53788960211921499, 'relu 50': 0.73253208451029195, 'tanh 50': 0.53870442238279459}
+# with open("final_results.pkl", "wb") as f:
+#     pkl.dump(final_results, f)
